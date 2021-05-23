75d7d21177033b7c8267b82849660bbe7463dcd6
David Mwendwa <klatzdavie19@gmail.com>
2021-05-23 21:12:26 +0300

    Created hosted Database with Atlas and connected it with Express App

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }).then(() => console.log('DB connection successfull!'))
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
