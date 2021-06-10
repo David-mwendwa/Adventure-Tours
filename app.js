e9c8381b7a240a97a2af4afdcf2094160ed42067
David Mwendwa <klatzdavie19@gmail.com>
2021-06-10 12:32:19 +0300

    refactor: better errors and refactor
    Moved error logic into its own class, appError and global error handling to errorController

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const moment = require('moment');
 const morgan = require('morgan');
 const express = require('express');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -27,26 +29,9 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Cant find ${req.originalUrl} on this server`
-  // })
-
-  const err = new Error(`Cant find ${req.originalUrl} on this server`);
-  err.status = 'fail';
-  err.statusCode = 404;
-
-  next(err);
+  next(new AppError(`Cant find ${req.originalUrl} on this server`));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
