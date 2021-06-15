52fda44e2c0554cf005982273b00c58f35928cea
David Mwendwa <klatzdavie19@gmail.com>
2021-06-15 18:42:10 +0300

    feat: handle errors outside express unhandled rejection

--- server.js
+++ server.js
@@ -4,24 +4,36 @@ dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
 mongoose
   .connect(process.env.DATABASE_LOCAL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-  }).then(() => console.log('MongoDB Compass connection successfull!'))
+  })
+  .then(() => console.log('MongoDB Compass connection successfull!'));
 
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-  }).then(() => console.log('MongoDB Atlas connection successfull!'))
-
+  })
+  .then(() => console.log('MongoDB Atlas connection successfull!'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`${process.env.NODE_ENV} server listening on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
