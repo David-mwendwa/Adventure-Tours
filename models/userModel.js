ccbeb2e6e43ca40a25bf000895b83877363a97d7
David Mwendwa <klatzdavie19@gmail.com>
2021-06-16 19:40:39 +0300

    feat: manage user passwords
    Validated if inputed password is equal to the confirmed password and enrypted password in order to secture against attacks
    Did the encryption using document middleware --> pre-save hook

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bycrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema({
   name: {
@@ -11,18 +12,37 @@ const userSchema = mongoose.Schema({
     required: [true, 'Please provide your email'],
     unique: true,
     lowercase: true,
-    validate: [validator.isEmail, 'Please provide a valid email']
+    validate: [validator.isEmail, 'Please provide a valid email'],
   },
   photo: String,
   password: {
     type: String,
     required: [true, 'Please provide a password'],
-    minlength: 8
+    minlength: 8,
   },
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
+    validate: {
+      // This only works on CREATE and SAVE!!!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords do not match',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  // Only run this function if passsword was actually modified
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bycrypt.hash(this.password, 12);
+  
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = new mongoose.model('User', userSchema);
